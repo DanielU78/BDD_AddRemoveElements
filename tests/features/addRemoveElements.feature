d164b462b19e032e809d0986a361f2d5668f8745
DanielU78 <89214138+DanielU78@users.noreply.github.com>
2021-12-11 21:27:43 +0200

    Initial commit

---
+++ tests/features/addRemoveElements.feature
@@ -0,0 +1,8 @@
+Feature: Herukoapp AddRemoveElements Testing
+    As a user,
+    I want to verify if when i click on AddElement
+    The Delete button appear
+
+    Scenario: Click the Add Element button
+    Given the AddRemoveElements home page is displayed
+    When the user click AddElement button
---
+++ tests/step_defs/test_search_steps.py
@@ -0,0 +1,21 @@
+from pytest_bdd import scenarios, when, then, given, parsers
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+# Constants
+ADDREMOVEELEMENTS_HOME = 'https://the-internet.herokuapp.com/add_remove_elements/'
+
+# Scenarios
+scenarios("../features/addRemoveElements.feature")
+
+# Shared Given Steps
+@given('the AddRemoveElements home page is displayed')
+def add_home(browser):
+    browser.get(ADDREMOVEELEMENTS_HOME)
+# When Steps
+
+@when('the user click AddElement button')
+def click_add_element_button(browser):
+    add_button = browser.find_element(By.CSS_SELECTOR, 'body>div>div>div>button')
+    add_button.click()
